33e6d6faffeed24d4f8b195766d7b6144b10e4bc
Chris Lu <luchris@google.com>
2023-12-05 05:36:32 +0000

    display: disable VRR for factory builds
    Bug: 314255124
    Test: VRR is enalbed in formal builds but disabled in factory
    Change-Id: I2bc142c75f69383ffa06291b21d0aaa5916e3df5

--- device-caiman.mk
+++ device-caiman.mk
@@ -224,9 +224,11 @@ PRODUCT_VENDOR_PROPERTIES += \
 # Display RRS default Config
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.vendor.display.primary.boot_config=960x2142@120:120
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.vendor.primarydisplay.vrr.enabled=true
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.vendor.primarydisplay.vrr.expected_present.headsup_ns=30000000
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.vendor.primarydisplay.vrr.expected_present.timeout_ns=500000000
+ifeq ($(filter factory_caiman, $(TARGET_PRODUCT)),)
+    PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.vendor.primarydisplay.vrr.enabled=true
+    PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.vendor.primarydisplay.vrr.expected_present.headsup_ns=30000000
+    PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.vendor.primarydisplay.vrr.expected_present.timeout_ns=500000000
+endif
 
 # Display OP HZ Config
 PRODUCT_VENDOR_PROPERTIES += \
