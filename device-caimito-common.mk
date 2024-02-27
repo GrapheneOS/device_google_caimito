e05a45ac1b65cdf1c64b43fb61847937e3022750
samou <samou@google.com>
2024-02-27 08:22:45 +0000

    bm: add mitigation config file for caimito
    Bug: 319619657
    Change-Id: I76cf52eaccfac00eb177752e5105eaabdae2e4c6
    Signed-off-by: samou <samou@google.com>

--- device-caimito-common.mk
+++ device-caimito-common.mk
@@ -39,3 +39,11 @@ PRODUCT_COPY_FILES += \
 	$(TARGET_VENDOR_PERF_CONFIG_PATH)/powerhint-$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 PRODUCT_COPY_FILES += \
 	$(TARGET_VENDOR_PERF_CONFIG_PATH)/powerhint-zuma.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint-proto.json
+
+# Battery Mitigation Config
+ifeq (,$(TARGET_VENDOR_BATTERY_MITIGATION_CONFIG_PATH))
+TARGET_VENDOR_BATTERY_MITIGATION_CONFIG_PATH := device/google/caimito/battery_mitigation
+endif
+
+PRODUCT_COPY_FILES += \
+	$(TARGET_VENDOR_BATTERY_MITIGATION_CONFIG_PATH)/bm_config_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/bm_config.json
