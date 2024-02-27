443a642059770c8f5379df2598dfe01b2c44d083
Bill Yi <byi@google.com>
2024-02-27 14:26:16 -0800

    Add AOSP support for caiman/komodo/tokay
    We will have a 'vendor' project containing proprietary blobs
    to be able to build and flash Pixel device when vendor source is not
    available.
    The project names are
    platform/vendor/aosp/google_devices/caiman
    platform/vendor/aosp/google_devices/komodo
    platform/vendor/aosp/google_devices/tokay
    
    and the path is
    vendor/google_devices/caiman
    vendor/google_devices/komodo
    vendor/google_devices/tokay
    
    BUG: 325612343
    Change-Id: I881994da5b81e963f5427af1642e29bffdca9c97

--- device-tokay.mk
+++ device-tokay.mk
@@ -20,6 +20,7 @@ TARGET_BOARD_KERNEL_HEADERS := device/google/caimito-kernel/kernel-headers
 $(call inherit-product-if-exists, vendor/google_devices/caimito/prebuilts/device-vendor-tokay.mk)
 $(call inherit-product-if-exists, vendor/google_devices/zumapro/prebuilts/device-vendor.mk)
 $(call inherit-product-if-exists, vendor/google_devices/zumapro/proprietary/device-vendor.mk)
+$(call inherit-product-if-exists, vendor/google_devices/tokay/proprietary/device-vendor.mk)
 $(call inherit-product-if-exists, vendor/google_devices/caimito/proprietary/tokay/device-vendor-tokay.mk)
 
 ifeq ($(filter factory_tokay, $(TARGET_PRODUCT)),)
