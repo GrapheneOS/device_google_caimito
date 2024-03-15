8aa55106fa5985d8e350e66208d312e6a6d563e9
Timothy Wang <timothywang@google.com>
2024-03-15 13:57:32 +0800

    Enable camera exif model/make reporting
    Reveal real model and make name in the exif.
    
    Test: CTS StillCaptureTest#testFocalLengths
    Bug: 319743372
    Change-Id: I95bb84e1ef2b6a8983a0ae09de476f0ed9db2115
    Merged-In: I95bb84e1ef2b6a8983a0ae09de476f0ed9db2115

--- device-caiman.mk
+++ device-caiman.mk
@@ -284,6 +284,10 @@ PRODUCT_VENDOR_PROPERTIES += \
 PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.camera.front_720P_always_binning=true
 
+# Enable camera exif model/make reporting
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.camera.exif_reveal_make_model=true
+
 # Display RRS default Config
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.vendor.display.primary.boot_config=960x2142@120:120
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.vendor.primarydisplay.preferred_mode=960x2142@120:120
