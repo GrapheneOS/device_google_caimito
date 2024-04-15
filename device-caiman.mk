a9af1a2b48d9f9c15622d1512d6ae1b54e545024
Benson Li <bensonli@google.com>
2024-04-15 12:23:16 +0000

    Add system property for concurrent encryption on LE and BR/EDR
    Bug: 330704060
    Change-Id: Idd154d27dedbb15262a4ce4d478fedaa6748b40f
    Merged-In: Idd154d27dedbb15262a4ce4d478fedaa6748b40f
    Test: make
    (cherry picked from commit ca94ed1a3d547262dc9c3640bfe6143409f153fb)

--- device-caiman.mk
+++ device-caiman.mk
@@ -380,6 +380,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
    persist.bluetooth.leaudio.allow_list=SM-R510
 
+# Support LE & Classic concurrent encryption (b/330704060)
+PRODUCT_PRODUCT_PROPERTIES += \
+    bluetooth.ble.allow_enc_with_bredr=true
+
 # Keyboard height ratio and bottom padding in dp for portrait mode
 PRODUCT_PRODUCT_PROPERTIES += \
           ro.com.google.ime.kb_pad_port_b=8 \
