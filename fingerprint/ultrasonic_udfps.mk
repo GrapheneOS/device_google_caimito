639a63c99829d9276bd48ff1391325b581e7bc80
Albert Chen <albchen@google.com>
2024-05-20 16:21:22 +0000

    Remove QFingerprintService from being included.
    Test: Compile and verify fingerprint functionality.
    Bug: 339342935
    Change-Id: Ic23494d9ba1aeb792489163dfd4cfbbd6dccc9b4

--- fingerprint/ultrasonic_udfps.mk
+++ fingerprint/ultrasonic_udfps.mk
@@ -15,12 +15,10 @@
 #
 
 PRODUCT_SOONG_NAMESPACES += \
-	vendor/qcom/fingerprint/qfp-service \
-	vendor/qcom/fingerprint/QFingerprintService \
+	vendor/qcom/fingerprint/qfp-service
 
 PRODUCT_PACKAGES += \
-	qfp-daemon \
-	QFingerprintService
+	qfp-daemon
 
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
