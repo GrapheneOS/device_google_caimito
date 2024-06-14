4ff1f700849f387436802ff3bb659ba9337c95a9
Chris Lu <luchris@google.com>
2024-06-14 13:03:56 +0000

    Display: disable NS mode and allow BTS follow operation rate
    Bug: 349703305
    Test: Smooth display on/off
    Test: Performance test
    Change-Id: I992914b7e0c0911d44a9d263dbb4839814f15b12
    Merged-In: I992914b7e0c0911d44a9d263dbb4839814f15b12
    (cherry picked from commit 58a1edecf201140817d4ffad21f007dbae94ab7c)

--- device-komodo.mk
+++ device-komodo.mk
@@ -93,10 +93,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.ignore_hdr_camera_layer
 # Display OP HZ Config
 PRODUCT_VENDOR_PROPERTIES += \
     vendor.primarydisplay.op.hs_hz=120 \
-    vendor.primarydisplay.op.ns_hz=60 \
-    vendor.primarydisplay.op.ns_min_dbv=440 \
-    vendor.primarydisplay.op.hs_switch_min_dbv=1088 \
-    vendor.primarydisplay.op.hist_delta_th=8
+    vendor.primarydisplay.op.ns_hz=120
 
 # Display fixed TE2
 PRODUCT_VENDOR_PROPERTIES += vendor.primarydisplay.fixed_te2.default_rate_hz=120
