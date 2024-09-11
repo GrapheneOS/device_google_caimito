0bf4e1ec54615f776c8b7c5439ec6f0cf2dcdd3d
Juan Yescas <jyescas@google.com>
2024-09-10 23:01:03 -0700

    16kb: Set data partition to f2fs for 16kb aosp caimito targets only
    Bug: 350101696
    Test: Built caimito targets and checked mounted partitions
    Flag: EXEMPT bugfix
    Change-Id: I40b8bb180ae9b23965ab379a63825542c121adde

--- aosp_komodo_16k.mk
+++ aosp_komodo_16k.mk
@@ -2,6 +2,5 @@ $(call inherit-product, device/google/caimito/aosp_komodo.mk)
 
 PRODUCT_NAME := aosp_komodo_16k
 
-TARGET_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_RW_FILE_SYSTEM_TYPE := ext4
+TARGET_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
 TARGET_BOOTS_16K := true
